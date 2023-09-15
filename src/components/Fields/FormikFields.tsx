a19df1bca3d47adc4de6291c96f3971525b284b1
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2023-09-15 18:05:46 +0700

    remove unsused code

--- src/components/Fields/FormikFields.tsx
+++ src/components/Fields/FormikFields.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
 import { IFieldsProps } from '../../types/components/Fields.types'
 import { Form, Input, InputProps } from 'antd'
 import { useField } from 'formik'
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsReducer from './feature/product-slice'
-import usersReducer from './feature/users-slice'
+
 import authReducer from './feature/auth-slice'
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
 
 export const store = configureStore({
-  reducer: { productsReducer, usersReducer, authReducer },
+  reducer: { productsReducer, authReducer },
 })
 
 export type RootState = ReturnType<typeof store.getState>
